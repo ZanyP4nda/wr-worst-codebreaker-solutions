1be8fe93a41f6fe83d742dc505f3a4c50e2223a9
ZanyPanda <64766002+ZanyP4nda@users.noreply.github.com>
2024-02-08 02:50:17 +0000

    stinky input done

--- morepaths/morepaths.cpp
+++ morepaths/morepaths.cpp
@@ -3,57 +3,53 @@
 using namespace std;
 
 vector<vector<int>> grid;
+vector<vector<pair<int, int>>> adj;
 
-int min_custom(int a, int b) {
-	if(a < 0 && b < 0) {
-		return 0;
-	}
-	if(a < 0 && b >= 0 || a > b) {
-		return b;
-	}
-//	if(b < 0 && a >= 0 || a < b) {
-//		return a;
-//	}
-	return a;
-}
-
-int max_custom(int a, int b) {
-	if(a < 0 && b < 0) {
-		return 0;
-	}
-	if(a < 0 && b >= 0 || b > a) {
-		return b;
-	}
-//	if(b < 0 && a >= 0 || a > b) {
-//		return a;
-//	}
-	return a;
-}
-
-int solve(int y, int x, int k) {
-	// cout << y << " " << x << "\n";
-	if(y < 0 || x < 0) {
-		return -1;
-	}
-	if(k > 0) {
-		cout << "k: " << k << " (" << grid[y][x] << ")\n";
-		return min_custom(solve(y - 1, x, k - 1), solve(y, x - 1, k - 1)) + grid[y][x];
-	} 
-	return max_custom(solve(y - 1, x, k - 1), solve(y, x - 1, k - 1)) + grid[y][x];
+int yens(int s, vector<vector<pair<int, int>>> adj) {
+	priority_queue<pair<int, int>> q;
+	q.push({0, s}); // visit the first node with starting weight
+	return 0;
 }
 
 int main() {
 	int n, m , k;
 	cin >> n >> m >> k;
+
+	// create grid
 	for(int i = 0; i < n; ++i) {
 		vector<int> row;
 		for(int j = 0; j < m; ++j) {
 			int x;
 			cin >> x;
 			row.push_back(x);
+
 		}
 		grid.push_back(row);
 	}
-	cout << solve(n - 1, m - 1, k - 1) << "\n";
+
+	// construct adjacency list
+	adj.resize(n * m);
+	for(int i = 0; i < n; ++i) {
+		for(int j = 0; j <= m; ++j) {
+			if(j > 0) {
+				adj[i * m + j - 1].push_back(make_pair(i * m + j, grid[i][j]));
+			}
+
+			if(i > 0) {
+				adj[(i - 1) * m + j].push_back(make_pair(i * m + j, grid[i][j]));
+			}
+		}
+	}
+
+//	print adjacency list
+//	int numNode = 0;
+//	for(auto node : adj) {
+//		for(auto edge : node) {
+//			cout << numNode << " " << edge.first << " " << edge.second << "\n";
+//		}
+//		++numNode;
+//	}
+
+	//cout << solve(n - 1, m - 1, k - 1) << "\n";
 	return 0;
 }
